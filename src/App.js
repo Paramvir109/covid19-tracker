c7b77a99027cfec660f9e36519172a81211d87fc
Paramvir <paramvir109@gmail.com>
2020-05-25 07:07:56 +0530

    Basic card and data fetched

--- src/App.js
+++ src/App.js
@@ -3,15 +3,26 @@ import React from 'react';
 // import Cards from './components/Cards/Cards';
 // import CountryPicker from './components/CountryPicker/CountryPicker';
 
+import  'antd/dist/antd.css';
 import { Cards, Charts,CountryPicker} from './components';
 import styles from './App.module.css';// So while applying classname use styles.className
+import { fetchData } from './api';
 
 class App extends React.Component {
 
+  state = {
+    data : {}
+  }
+  async componentDidMount(){
+    const fetchedData = await fetchData();
+    this.setState({data : fetchedData});
+  }
+
   render() {
+    const data = this.state.data;
     return (
       <div className={styles.container}> 
-        <Cards />
+        <Cards data = {data}/>
         <CountryPicker />
         <Charts />
 
--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,9 +1,42 @@
 import React from 'react';
+import styles from './Cards.module.css';
+import { Card, Col, Row,Typography   } from 'antd';
 
-const Cards = () => {
-    return(
-        <div>
-            Cards
+const { Text,Title } = Typography;
+
+
+const Cards = ({data : {confirmed, recovered,deaths,lastUpdate}}) => {
+    if(!confirmed){
+        return "Loading";
+    }
+    return (
+        <div className={styles.container}>
+            <Row gutter={24} justify={"center"}>
+                <Col span={8}>
+                    <Card >
+                        <Text type="secondary">Infected</Text>
+                            <Title level={4}>{confirmed.value}</Title>
+                        <Text type="secondary">Real Date</Text>< br/>
+                        <Text>No. of active cases of COVID-19</Text>
+                    </Card>
+                </Col>
+                <Col span={8}>
+                    <Card >
+                        <Text type="secondary">Recovered</Text>
+                        <Title level={4}>{recovered.value}</Title>
+                        <Text type="secondary">Real Date</Text>< br/>
+                        <Text>No. of recoveries from COVID-19</Text>
+                    </Card>
+                </Col>
+                <Col span={8}>
+                    <Card >
+                        <Text type="secondary">Deaths</Text>
+                        <Title level={4}>{deaths.value}</Title>
+                        <Text type="secondary">Real Date</Text>< br/>
+                        <Text>No. of deaths caused by COVID-19</Text >
+                    </Card>
+                </Col>
+            </Row>
         </div>
     )
 }
